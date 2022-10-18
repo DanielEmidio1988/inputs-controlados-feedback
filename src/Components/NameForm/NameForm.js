9cd7514bb935d36dc5101daad440c5c6195e1913
DanielEmidio1988 <daniel.emidio.silva@gmail.com>
2022-10-18 18:18:05 -0300

    Entrega Exercicio

--- src/Components/NameForm/NameForm.js
+++ src/Components/NameForm/NameForm.js
@@ -6,21 +6,22 @@ const NameForm = (props) => {
     <Form>
         <label>
           Nome:
-          <Input  placeholder="Nome" value={name} onChange={onChangeName}/>
+          <Input  placeholder="Nome" value={props.name} onChange={props.onChangeName} required/>
         </label>
         <label>
           Idade:
-          <Input  placeholder="Idade" value={age} onChange={onChangeAge}/>
+          <Input  placeholder="Idade" value={props.age} onChange={props.onChangeAge} required/>
         </label>
         <label>
           E-mail:
-          <Input  placeholder="usuario@usuario.com" value={email} onChange={onChangeEmail}/>
+          <Input  placeholder="usuario@usuario.com" value={props.email} onChange={props.onChangeEmail} required/>
         </label>
         <label>
           Confirmação de e-mail:
-          <Input  placeholder="usuario@usuario.com" value={email} onChange={onChangeEmail}/>
+          <Input  placeholder="usuario@usuario.com" value={props.validaEmail} onChange={props.onChangeValidaEmail}/>
         </label>
-      <button onClick={sendData}>Enviar dados</button>
+
+      <button onClick={props.sendData}>Enviar dados</button>
       </Form>
   )
 }
